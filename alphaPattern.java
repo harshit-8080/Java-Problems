ee3f4113486c95ad42b7be390ba6a5964988b4f2
harshit71 <harshit.raj@itential.com>
2022-08-20 23:05:30 +0530

    adding alpha pattern

---
+++ alphaPattern.java
@@ -0,0 +1,41 @@
+/*
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+GGGGGGG
+ */
+
+import java.util.*;
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int i = 1;
+        
+        
+        while(i<=n){
+            char ch = (char)('A' + i - 1);
+            
+            int j = 1;
+            
+            while (j<=i){
+                
+                System.out.print(ch);
+                j++;
+                
+            }
+             i++;
+            System.out.println();
+        }
+
+		
+	}
+
+}
