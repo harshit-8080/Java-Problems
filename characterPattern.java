c82177a3d541fa7a9b7fed18489c2f5615a2a5dc
harshit71 <harshit.raj@itential.com>
2022-08-20 23:07:06 +0530

    character pattern

---
+++ characterPattern.java
@@ -0,0 +1,40 @@
+/*
+A
+BC
+CDE
+DEFG
+EFGHI
+ */
+
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int i = 1;
+        
+        while(i<=n) {
+            
+            char ch = (char)('A' + i - 1);
+            
+            int j = 1;
+            
+            while(j<=i) {
+                
+                System.out.print(ch);
+                ch = (char)(ch + 1);
+                j++;
+            }
+            
+            i++;
+            System.out.println();
+        }
+		
+	}
+
+}
